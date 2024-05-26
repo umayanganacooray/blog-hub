e8a65d5398fb11e9094a60820dcb409af62f1967
umayanaganacooray <ume.cooray@gmail.com>
2024-05-26 11:38:46 +0530

    AWS S3 bucket setup

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,14 @@
-const {PORT, CONNECTION_URL, JWT_SECRET, SENDER_EMAIL, EMAIL_PASSWORD} = process.env;
+const {
+    PORT, 
+    CONNECTION_URL, 
+    JWT_SECRET, 
+    SENDER_EMAIL, 
+    EMAIL_PASSWORD,
+    AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_BUCKET_NAME,
+    AWS_REGION
+} = process.env;
 
 module.exports = {
     port : PORT, 
@@ -6,4 +16,8 @@ module.exports = {
     jwtSecret: JWT_SECRET,
     senderEmail: SENDER_EMAIL,
     emailPassword: EMAIL_PASSWORD,
+    awsAccessKey: AWS_ACCESS_KEY,
+    awsSecretAccessKey: AWS_SECRET_ACCESS_KEY,
+    awsBucketName: AWS_BUCKET_NAME,
+    awsRegion: AWS_REGION,
 };
