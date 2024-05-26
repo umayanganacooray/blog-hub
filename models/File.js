12d7031ce36e3256b7ba8a9dcd8a0811abebd275
umayanaganacooray <ume.cooray@gmail.com>
2024-05-26 12:08:04 +0530

    Creating File model

---
+++ models/File.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const fileSchema = mongoose.Schema({
+    key: {type: String, required: true},
+    size: Number,
+    mimetype: String,
+    createdBy: {type: mongoose.Types.ObjectId, ref: "user"}
+},{timeatamps: true}
+);
+
+const File = mongoose.model("file", fileSchema);
+module.exports = File;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require("./User");
 const Category = require("./Category");
+const File = require("./File");
 
-module.exports = {User, Category} ;
+module.exports = {User, Category, File} ;
