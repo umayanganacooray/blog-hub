f3a7f23dc94965f4db1c32d9d515af7039b9144b
umayanaganacooray <ume.cooray@gmail.com>
2024-07-17 05:33:35 +0530

    logout functionality

--- frontend/src/components/PrivateNavbar.jsx
+++ frontend/src/components/PrivateNavbar.jsx
@@ -1,6 +1,20 @@
 import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const PrivateNavbar = () =>{
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        window.localStorage.removeItem("blogData");
+        toast.success("Logout successfull", {
+            position: "top-right",
+            autoClose: true
+        });
+        navigate("/login");
+    };
+
     return(
         <nav className="primary-link">
             <NavLink to="/">Home</NavLink>
@@ -8,7 +22,7 @@ const PrivateNavbar = () =>{
             <NavLink to="/posts">Posts</NavLink>
             <NavLink to="/profile">Profile</NavLink>
             <NavLink to="/setting">Setting</NavLink>
-            <NavLink to="/login">Logout</NavLink>
+            <NavLink to="/login" onClick={handleLogout}>Logout</NavLink>
         </nav>
         
     );
